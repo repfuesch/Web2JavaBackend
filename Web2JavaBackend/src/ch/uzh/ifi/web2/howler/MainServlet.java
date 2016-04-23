780f21ae60d8663390e25375a260bb0b0e86b58d
flo <repfuesch@gmail.com>
2016-04-23 13:40:10 +0200

    Initial Commit

---
+++ Web2JavaBackend/src/ch/uzh/ifi/web2/howler/MainServlet.java
@@ -0,0 +1,52 @@
+package ch.uzh.ifi.web2.howler;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ch.uzh.ifi.feedback.library.rest.IRestManager;
+import ch.uzh.ifi.feedback.library.rest.RestManager;
+
+/**
+ * Servlet implementation class MainServlet
+ */
+@WebServlet("/MainServlet")
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private IRestManager restManager;
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MainServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    private void InitController(){
+    	
+    	restManager = new RestManager();
+        
+        try{
+        	restManager.Init("ch.uzh.ifi.web2.howler");
+        }
+        catch(Exception ex){
+        	System.out.println(ex.getMessage());
+        	ex.printStackTrace();
+        	super.destroy();
+        }
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		restManager.Get(request, response);
+	}
+
+}
